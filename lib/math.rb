d4e8e8ca6d28754b99c4fc5a657f0524ddc2faec
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-05-06 03:56:39 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+  num1 + num2
 end
+
+addition(5,4)
 
 def subtraction(num1, num2)
-
+  num1 - num2
 end
+
+subtraction(3,2)
 
 def division(num1, num2)
-
+  num1 / num2
 end
+
+division(12,6)
 
 def multiplication(num1, num2)
-
+  num1 * num2
 end
+
+multiplication(100,100)
 
 def modulo(num1, num2)
-
+  num1 % num2
 end
+
+modulo(4,10)
 
 def square_root(num)
-
+  Math.sqrt(num)
 end
+
+square_root(81)
